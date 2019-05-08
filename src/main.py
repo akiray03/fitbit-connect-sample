9498b217eb7b25d947f7486953ebcc0512155a00
Akira Yumiyama <yumiyama.akira@gmail.com>
2019-05-08 18:22:29 +0900

    [from now] 2019/05/08 18:22:29
    diff --git a/src/main.py b/src/main.py
    index df785ca..5af2284 100644
    --- a/src/main.py
    +++ b/src/main.py
    @@ -1,13 +1,32 @@
     import flask
     import os
    +import logging
     from fitbit import Fitbit
     from urllib.parse import urljoin
    +import sys
    +
    +logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
    
     app = flask.Flask(__name__)
    +app.secret_key = 'too long secret'
    
     redirect_path = '/callback'
    
    +"""
    +DEBUG:requests_oauthlib.oauth2_session:Response headers were {
    +'Date': 'Wed, 08 May 2019 09:17:41 GMT', 'Content-Type': 'application/json;charset=utf-8', 'Transfer-Encoding': 'chunked',
    + 'Connection': 'keep-alive', 'Vary': 'Origin,Accept-Encoding', 'Cache-control': 'no-cache, private',
    + 'WWW-Authenticate': 'Basic realm="api.fitbit.com"', 'Content-Language': 'ja-JP', 'Content-Encoding': 'gzip',
    +'X-Frame-Options': 'SAMEORIGIN, SAMEORIGIN', 'Expect-CT': 'max-age=604800, report-uri="https://report-uri.cloudflare.com/cdn-cgi/beacon/expect-ct"',
    +'Server': 'cloudflare', 'CF-RAY': '4d3a578bec34a5c4-NRT'}
    +and content {"errors":[{"errorType":"invalid_client",
    +"message":"Invalid authorization header format. The client id was not provided in proper format inside Authorization Header.
    +Received authorization header = Basic MjJEUFFYOg==,  received client encoded id = null.
    +Visit https://dev.fitbit.com/docs/oauth2 for more information on the Fitbit Web API authorization process."}],
    +"success":false}.
    +"""
    +
    
     @app.route('/')
     def hello():
    @@ -22,7 +41,8 @@ def login():
             redirect_uri=urljoin(flask.request.host_url, redirect_path)
         )
         print(urljoin(flask.request.host_url, redirect_path))
    -    url, _ = fitbit.client.authorize_token_url(scope=None)
    +    url, state = fitbit.client.authorize_token_url(scope=None)
    +    flask.session['login_state'] = state
         print(f'redirect url is {url}')
         return flask.redirect(url)
    
    @@ -32,7 +52,8 @@ def callback():
         fitbit = Fitbit(
             client_id=os.environ['FITBIT_CLIENT_ID'],
             client_secret=os.environ['FITBIT_CLIENT_SECRET'],
    -        redirect_uri=urljoin(flask.request.host_url, redirect_path)
    +        redirect_uri=urljoin(flask.request.host_url, redirect_path),
    +        state=flask.session['login_state']
         )
         print(flask.request.args.get('code'))
         fitbit.client.fetch_access_token(code=flask.request.args.get('code'))

--- src/main.py
+++ src/main.py
@@ -1,12 +1,31 @@
 import flask
 import os
+import logging
 from fitbit import Fitbit
 from urllib.parse import urljoin
+import sys
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 
 app = flask.Flask(__name__)
+app.secret_key = 'too long secret'
 
 redirect_path = '/callback'
+
+"""
+DEBUG:requests_oauthlib.oauth2_session:Response headers were {
+'Date': 'Wed, 08 May 2019 09:17:41 GMT', 'Content-Type': 'application/json;charset=utf-8', 'Transfer-Encoding': 'chunked',
+ 'Connection': 'keep-alive', 'Vary': 'Origin,Accept-Encoding', 'Cache-control': 'no-cache, private', 
+ 'WWW-Authenticate': 'Basic realm="api.fitbit.com"', 'Content-Language': 'ja-JP', 'Content-Encoding': 'gzip',
+'X-Frame-Options': 'SAMEORIGIN, SAMEORIGIN', 'Expect-CT': 'max-age=604800, report-uri="https://report-uri.cloudflare.com/cdn-cgi/beacon/expect-ct"', 
+'Server': 'cloudflare', 'CF-RAY': '4d3a578bec34a5c4-NRT'} 
+and content {"errors":[{"errorType":"invalid_client",
+"message":"Invalid authorization header format. The client id was not provided in proper format inside Authorization Header. 
+Received authorization header = Basic MjJEUFFYOg==,  received client encoded id = null. 
+Visit https://dev.fitbit.com/docs/oauth2 for more information on the Fitbit Web API authorization process."}],
+"success":false}.
+"""
 
 
 @app.route('/')
@@ -22,7 +41,8 @@ def login():
         redirect_uri=urljoin(flask.request.host_url, redirect_path)
     )
     print(urljoin(flask.request.host_url, redirect_path))
-    url, _ = fitbit.client.authorize_token_url(scope=None)
+    url, state = fitbit.client.authorize_token_url(scope=None)
+    flask.session['login_state'] = state
     print(f'redirect url is {url}')
     return flask.redirect(url)
 
@@ -32,7 +52,8 @@ def callback():
     fitbit = Fitbit(
         client_id=os.environ['FITBIT_CLIENT_ID'],
         client_secret=os.environ['FITBIT_CLIENT_SECRET'],
-        redirect_uri=urljoin(flask.request.host_url, redirect_path)
+        redirect_uri=urljoin(flask.request.host_url, redirect_path),
+        state=flask.session['login_state']
     )
     print(flask.request.args.get('code'))
     fitbit.client.fetch_access_token(code=flask.request.args.get('code'))
